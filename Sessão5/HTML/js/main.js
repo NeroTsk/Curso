2dcc0f1f4e59865d7d77ed9b31851710e9ada956
lucasrodrigues88 <lucasrodrigues@letsgrow.com.br>
2019-03-26 10:09:43 -0300

    realizado operações logicas e declaração de variaveis com java

---
+++ Sessão5/HTML/js/main.js
@@ -0,0 +1,19 @@
+var a = 4;
+var b = 5;
+var c = 'character';
+var d = "Here have a buch of text";
+var mycar = "mazda";
+var mycolor = "silver";
+var e = true; /// true or false
+
+document.write(a*b);
+document.write('<br>');
+document.write(mycar+' '+mycolor);
+document.write('<br>');
+document.write(d);
+
+if(!d){
+    console.log('d is false');
+}else{
+    console.log('d is true');
+}
