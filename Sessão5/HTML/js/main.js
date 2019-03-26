290fef498cf3bc094e6eebec1992911487e68e82
lucasrodrigues88 <lucasrodrigues@letsgrow.com.br>
2019-03-26 17:29:20 -0300

    feito primeiro objeto em java

--- Sessão5/HTML/js/main.js
+++ Sessão5/HTML/js/main.js
@@ -34,12 +34,46 @@ var output = document.getElementById('output');
 //firstName = "Mike";
 //var myFullName = myName(firstName, lastName);
 //output.innerHTML = myFullName;
-
-var blank = '';
+function htmlOutput(myOutput){
+    output.innerHTML=myOutput; 
+}
+var person = {
+    age: 25,
+    firstName: "John",
+    lastName: "Smith",
+    fullName: function() {
+    return this.firstName + " " + this.lastName;
+    }
+    };
+    person.firstName = "Mike";
+    htmlOutput(person.fullName());
+    function pickArray(a) {
+    return myArray[a];
+    }
+    function myName(a, b) {
+    var fullName = a + ' ' + b; // local fullName
+    return fullName;
+    }
+    function htmlOutput(myOutput) {
+    output.innerHTML = myOutput;
+    }
+/*var blank = '';
 var firstName = 'Nero';
 var lastName = 'tsk';
-var array = ['j','o','h','n'];
+var myArray = ['first item', 'second', 3, 4, 5, 'six'];
+var one = '1';
+var two = 2;
+console.log(myArray);
+myArray.push ('seven','eight','nine',10);
+myArray[2] = "NEW UPDATED";
+console.log(myArray);
+htmlOutput(pickArray(6));
 
+function pickArray(a){
+    return myArray[a];
+}
+*/
+/*
 function myName(a,b){
   var fullName = a+' '+b;
   output.innerHTML = fullName;
@@ -61,7 +95,7 @@ myName(firstName,lastName);
   }
   
   htmlOutput(1);
-*/
+
 
 function myChoice(a){
     var b = ' ';
@@ -77,3 +111,4 @@ function myChoice(a){
   
    var myrandom = Math.random();
    htmlOutput(myrandom+ '<br>' +myChoice(myrandom));
+   */
