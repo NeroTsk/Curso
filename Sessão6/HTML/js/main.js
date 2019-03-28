73bde960974d4d1c737629f32185b45a1c05c3de
lucasrodrigues88 <lucasrodrigues@letsgrow.com.br>
2019-03-28 13:44:57 -0300

    Realizado com jquery função com attr e prop

--- Sessão6/HTML/js/main.js
+++ Sessão6/HTML/js/main.js
@@ -3,12 +3,22 @@
             console.log($(this));
             console.log($('a').last().attr('href'));
             console.log($(this).attr('src'))
-        })
+        });
         $('nav a[href^="'+location.pathname.split('HTML/')[1]+'"]').addClass('active')
         $('p').click(function(){
             console.log($(this));
             console.log($(this)[0].className);
-            $(this).css({"background-color":"purple","font-size":"3em"})
+        $(this).css({"background-color":"purple","font-size":"3em"});
+        });
+        $('#myInput').change(function(){
+            console.log($(this).val());
+            console.log($('#checkBx'))
+            if($(this).val()== 'yes'){
+                $('#checkBx').prop("checked", true);
+            }else{
+                $('#checkBx').prop("checked", false);
+            }
+        });
             /*
             console.log($(this).hasClass('yellow'));
             if(($(th is).hasClass('yellow'))){
@@ -19,6 +29,4 @@
                 console.log("so long")
             }
             $(this).toggleClass('yellow')*/
-        });
-
     })
