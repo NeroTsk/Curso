2155cce7ed3c12645387374beecb55dc7d6110a2
lucasrodrigues88 <lucasrodrigues@letsgrow.com.br>
2019-03-28 11:12:34 -0300

    realizado ações com atributos no jquery

--- Sessão6/HTML/js/main.js
+++ Sessão6/HTML/js/main.js
@@ -1,12 +1,17 @@
     $(document).ready(function(){
-  
+        $('img').click(function(){
+            console.log($(this));
+            console.log($('a').last().attr('href'));
+            console.log($(this).attr('src'))
+        })
+        $('nav a[href^="'+location.pathname.split('HTML/')[1]+'"]').addClass('active')
         $('p').click(function(){
             console.log($(this));
             console.log($(this)[0].className);
             $(this).css({"background-color":"purple","font-size":"3em"})
             /*
             console.log($(this).hasClass('yellow'));
-            if(($(this).hasClass('yellow'))){
+            if(($(th is).hasClass('yellow'))){
                 //$(this).removeClass('yellow');
                 console.log("hey")
             }else{
