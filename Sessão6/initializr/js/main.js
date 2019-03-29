c368356e3ebdf6c9fd902a765aec802ca10fa89c
Thiago Sak <thiagosak@live.com>
2019-03-29 01:23:11 -0300

    pego exemplo de mundaças a partir do iframe pelo jquery

--- Sessão6/initializr/js/main.js
+++ Sessão6/initializr/js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
     console.log($("#myFrame").contents());
-    $("li").last().css("background-color","orange");
-   /* $("#myFrame").contents().find("div").css("background-color","red");
-    $("#arrayme").click(function(){
+
+    $("li").last().css("background-color", "#000000");
+
+    $("#myFrame").contents().find("div").css("background-color","red");
+    $('#arrayme').click(function(){
         console.log($(".one").children().toArray());
-    });
-    $(".one").find(".four").css("border","1px solid green");   */
-    console.log($('#myFrame').contents());
-})
+        });
+});
