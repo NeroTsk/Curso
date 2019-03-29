0f535ef6e028c1099968d29d3a5a753b2cafc492
lucasrodrigues88 <lucasrodrigues@letsgrow.com.br>
2019-03-29 09:52:56 -0300

    realizado efeitos de aparecer e desaparecer

--- Sessão6/initializr/js/main.js
+++ Sessão6/initializr/js/main.js
@@ -1,10 +1,21 @@
-$(document).ready(function() {
-    console.log($("#myFrame").contents());
-
-    $("li").last().css("background-color", "#000000");
-
-    $("#myFrame").contents().find("div").css("background-color","red");
-    $('#arrayme').click(function(){
+$(document).ready(function(){
+    $("tr:even").css("background-color", "orange");
+    $("a").click(function(){
+        event.preventDefault();
+        $("#output").html("the link you clicked was going to " + $(this).attr('href'));
+    });
+    $("#show").click(function(){
+        $(".main").show(1000);
+    });
+    $("#hide").click(function(){
+        $(".main").hide(1000);
+    })
+   // console.log($("#myFrame").contents());
+    //$("li").last().css("background-color","orange");
+   /* $("#myFrame").contents().find("div").css("background-color","red");
+    $("#arrayme").click(function(){
         console.log($(".one").children().toArray());
-        });
-});
+    });
+    $(".one").find(".four").css("border","1px solid green");   */
+    //console.log($('#myFrame').contents());
+})
