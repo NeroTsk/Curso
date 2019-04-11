b28a1c810f02faef610940d69fd1f7cdb0de945a
lucasrodrigues88 <lucasrodrigues@letsgrow.com.br>
2019-04-11 17:41:35 -0300

    Modificado NavBar para que fique mais responsivo a determinada situação

--- Projeto/index.php
+++ Projeto/index.php
@@ -3,15 +3,16 @@
 ?>
 <main class="inner cover h-100" role="main">
 
-	<h1 class="text-center">Welcome guest</h1>
-	<hr>
+
+	
 	<div class="row">
+		<!--<div class="col-md-6">
+			<iframe width="680" height="415" src="https://www.youtube.com/embed/7-Yu08ad2lg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>-->
 		<div class="col-md-6">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/7-Yu08ad2lg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		</div>
-		<div class="col-md-6">
-			<h2 class="text-center">Enjoy you stay</h2>
-			<h3 class="text-center">
+			<h1 class="text-center m">Welcome guest</h1>
+			<h2 class="text-center ">Enjoy you stay</h2>
+			<h3 class="text-center m">
 				This is a simple page created by me, 
 				here you will find content on topics of 
 				cyberpunk gender and technology in relation
